5eea8ce3ed990b05a4229bc857c7a13471af179f
Yeongkyeong12 <ohkyeong1995@gmail.com>
2021-11-25 21:53:12 +0900

    Design: Added style for Button

---
+++ styling-with-styled-components/src/components/Button.js
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  /* 공통 스타일*/
+  display: inline-flex;
+  outline: none;
+  border: none;
+  border-radius: 4px;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+  padding: 0 1rem;
+
+  /* 크기 */
+  height: 2.25rem;
+  font-size: 1rem;
+
+  /* 색상 */
+  background: #228be6;
+  &:hover {
+    background: #339af0;
+  }
+  &:active {
+    background: #1c7ed6;
+  }
+`;
+
+function Button() {
+  return <div></div>;
+}
+
+export default Button;
